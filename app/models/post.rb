5d139140e322b6c07bc6145fde039e4c93f9d22f
takaiken <you@example.com>
2021-08-21 03:11:00 +0000

    画像アップロード

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,6 @@ class Post < ApplicationRecord
     validates :title, presence: true, length: { minimum: 3, message: 'Too short to post!' }
     validates :body, presence: true
 
+    mount_uploader :image, ImageUploader
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_19_051119) do
+ActiveRecord::Schema.define(version: 2021_08_21_024229) do
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "body"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2021_08_19_051119) do
     t.text "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "image"
   end
 
   add_foreign_key "comments", "posts"
