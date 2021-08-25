ac1adcaefe77e128d0b260de82ed527a13d74396
takaiken <you@example.com>
2021-08-25 06:35:07 +0000

    ゲストログイン

--- Gemfile
+++ Gemfile
@@ -21,6 +21,8 @@ gem 'jbuilder', '~> 2.7'
 gem 'carrierwave'
 
 gem 'mini_magick'
+
+gem 'devise'
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 4.0'
 # Use Active Model has_secure_password
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,23 @@
 Rails.application.routes.draw do
+  
+
+    get 'home/index'
+    get 'home/show'
+    post '/home/guest_sign_in', to: 'home#guest_sign_in'
+  
+    devise_for :users, :controllers => {
+      :registrations => 'users/registrations',
+      :sessions => 'users/sessions'} 
+
+  devise_scope :user do
+    get "sign_in", :to => "users/sessions#new"
+    get "sign_out", :to => "users/sessions#destroy" 
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   resources :posts do
     resources :comments, only: [:create, :destroy]
   end
   
-  root 'posts#index'
+  root 'home#index'
 end
